7f0ed011c696574ec32a703d7653732a923814dc
sheepjune-jp <jun8547@gmail.com>
2025-04-15 00:12:55 +0900

    [Gold V] Title: 사과나무, Time: 108 ms, Memory: 120944 KB -BaekjoonHub

---
+++ 백준/Gold/19539. 사과나무/사과나무.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+h = list(map(int, input().split()))
+
+cnt = 0
+if sum(h) % 3 == 0:
+    for i in h:
+        cnt += i // 2
+    if cnt >= (sum(h) / 3):
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")
