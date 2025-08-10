ebbf456136a8b81b37299314d7f8f5b8ad7ad028
sheepjune-jp <jun8547@gmail.com>
2025-08-10 13:21:57 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 232 ms, Memory: 23816 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,22 +1,53 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int suNo = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
-        long S[]=new long[suNo + 1];
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i=1;i<=suNo;i++){
-            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+    static class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        FastScanner(InputStream is) { this.in = is; }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
         }
-        for (int q=0;q<quizNo;q++){
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j]-S[i-1]);
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do { c = read(); } while (c <= 32); 
+            if (c == '-') { sign = -1; c = read(); }
+            while (c > 32) {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        int suNo = fs.nextInt();
+        int quizNo = fs.nextInt();
+
+        long[] S = new long[suNo + 1];
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + fs.nextInt(); 
+        }
+
+        StringBuilder sb = new StringBuilder(quizNo * 3); 
+        for (int q = 0; q < quizNo; q++) {
+            int i = fs.nextInt();
+            int j = fs.nextInt();
+            sb.append(S[j] - S[i - 1]).append('\n');
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
