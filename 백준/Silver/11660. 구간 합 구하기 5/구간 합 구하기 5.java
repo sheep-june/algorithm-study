ca46088c92a35fb69a6f3bf92307fc80428af619
sheepjune-jp <jun8547@gmail.com>
2025-08-11 13:01:00 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 728 ms, Memory: 119528 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,27 +8,25 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int A[][] = new int[N + 1][N + 1];
+        int[][] D = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                A[i][j] = Integer.parseInt(st. nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + v; 
             }
         }
-        int D[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
-            }
-        }
-        for (int i = 0; i < M; i++) {
+
+        StringBuilder sb = new StringBuilder(M * 8);
+        for (int k = 0; k < M; k++) {
             st = new StringTokenizer(br.readLine());
-            int xl = Integer.parseInt(st.nextToken());
-            int yl = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int result = D[x2][y2] - D[xl - 1][y2] - D[x2][yl - 1] + D[xl - 1][yl - 1];
-            System.out.println(result);
+            int res = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            sb.append(res).append('\n');
         }
+        System.out.print(sb.toString());
     }
 }
