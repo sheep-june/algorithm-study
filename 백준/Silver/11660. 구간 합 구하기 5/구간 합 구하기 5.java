d08d53c8b44f4c2c4a1d379f38082397ceba19c7
sheepjune-jp <jun8547@gmail.com>
2025-08-11 13:01:43 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 268 ms, Memory: 29680 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,31 +1,39 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+    static class FastScanner {
+        private final InputStream in; private final byte[] buf = new byte[1<<16];
+        private int ptr=0,len=0;
+        FastScanner(InputStream is){ in=is; }
+        private int read() throws IOException {
+            if (ptr>=len){ len=in.read(buf); ptr=0; if (len<=0) return -1; }
+            return buf[ptr++];
+        }
+        int nextInt() throws IOException {
+            int c, s=1, v=0;
+            do{ c=read(); } while (c<=32);
+            if (c=='-'){ s=-1; c=read(); }
+            while (c>32){ v=v*10 + (c-'0'); c=read(); }
+            return v*s;
+        }
+    }
 
-        int[][] D = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                int v = Integer.parseInt(st.nextToken());
-                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + v; 
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        int N = fs.nextInt(), M = fs.nextInt();
+
+        int[][] D = new int[N+1][N+1];
+        for (int i=1;i<=N;i++){
+            for (int j=1;j<=N;j++){
+                int v = fs.nextInt();
+                D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + v;
             }
         }
 
-        StringBuilder sb = new StringBuilder(M * 8);
-        for (int k = 0; k < M; k++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int res = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-            sb.append(res).append('\n');
+        StringBuilder sb = new StringBuilder(M*8);
+        for (int k=0;k<M;k++){
+            int x1=fs.nextInt(), y1=fs.nextInt(), x2=fs.nextInt(), y2=fs.nextInt();
+            sb.append(D[x2][y2]-D[x1-1][y2]-D[x2][y1-1]+D[x1-1][y1-1]).append('\n');
         }
         System.out.print(sb.toString());
     }
