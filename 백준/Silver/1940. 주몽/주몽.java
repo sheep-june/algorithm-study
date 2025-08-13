7b15314a5735e9c361c90797bfd78414012c0b19
sheepjune-jp <jun8547@gmail.com>
2025-08-13 16:41:32 +0900

    [Silver IV] Title: 주몽, Time: 124 ms, Memory: 15332 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,28 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(buff.readLine());
-        int M = Integer.parseInt(buff.readLine());
-        int[] A = new int[N];
-        StringTokenizer strtn = new StringTokenizer(buff.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(strtn.nextToken());
-        }
-        Arrays.sort(A);
-        int count = 0;
-        int i=0;
-        int j = N-1;
-        while(i<j){
-            if(A[i]+A[j]<M)i++;
-            else if(A[i]+A[j]>M)j--;
-            else{
-                count++;
-                i++;
-                j--;
+    static final class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        FastScanner(InputStream is) { this.in = is; }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
             }
+            return buffer[ptr++];
         }
-        System.out.println(count);
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do { c = read(); } while (c <= ' ');   
+            if (c == '-') { sign = -1; c = read(); }
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        int N = fs.nextInt();
+        int M = fs.nextInt();
+
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) A[i] = fs.nextInt();
+
+        Arrays.sort(A); 
+
+        int i = 0, j = N - 1, count = 0;
+        while (i < j) {
+            int s = A[i] + A[j];
+            if (s < M) i++;
+            else if (s > M) j--;
+            else { count++; i++; j--; }
+        }
+
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        out.write(Integer.toString(count));
+        out.flush();
     }
 }
